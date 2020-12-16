c9de6e932f83d1bc2e7b9237395f4af3edd63eee
fidelhen <fidelhen@gmail.com>
2020-12-16 18:54:02 -0500

    Added slider value with JS and about us page

--- Web/index.js
+++ Web/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-
+    //Slider
+    $(this).on('input change', '#salaryRange', function() {
+        var x = $(this).val();
+        $("#salaryValue").html(x.toString().replace(/,/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")); 
+    });
+  
+    //Tabs in index.html
     $("#pills-occupation-tab").click(function(){
         $('#pills-occupation-tab').tab('show');
     });
@@ -7,4 +13,13 @@ $(document).ready(function(){
     $("#pills-rich-tab").click(function(){
         $('#pills-rich-tab').tab('show');
     });
+
+    //Dyanamic footer
+    var docHeight = $(window).height();
+    var footerHeight = $('#footer').outerHeight();
+    var footerTop = $('#footer').position().top + footerHeight;
+
+    if (footerTop < docHeight) {
+    $('#footer').css('margin-top', (docHeight - footerTop) + 'px');
+    }
 });
