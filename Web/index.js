053094842bab6fa927c5df999608422289462ee1
fidelhen <fidelhen@gmail.com>
2020-12-16 14:33:11 -0500

    Add tab bar for main features

--- Web/index.js
+++ Web/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+
+    $("#pills-occupation-tab").click(function(){
+        $('#pills-occupation-tab').tab('show');
+    });
+
+    $("#pills-rich-tab").click(function(){
+        $('#pills-rich-tab').tab('show');
+    });
 });
